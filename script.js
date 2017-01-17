150df2f3c448bbad565511d2537c351667ec5ba1
Scott Fox <scottfox5@gmail.com>
2017-01-16 21:17:10 -0600

    Put color counters in an object so their values can be accessed and incremented more dynamically.

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-$(function(){
-  var redCounter = 0;
-  var yellowCounter = 0;
-  var blueCounter = 0;
-  var greenCounter = 0;
-  $('button').on('click', function(){
-    var color = $(this).data('color');
-    $('.container').append('<div class="color-cube '+color+'"></div');
-    if (color == 'red'){
-      redCounter++;
-      $('#'+color).text(redCounter);
-    } else if (color == 'yellow'){
-      yellowCounter++;
-      $('#'+color).text(yellowCounter);
-    } else if (color == 'blue'){
-      blueCounter++;
-      $('#'+color).text(blueCounter);
-    } else if (color == 'green'){
-      greenCounter++;
-      $('#'+color).text(greenCounter);
-    };
+var colorCount = {//object of color counters so their value can be accessed and incremented dynamically 
+   red: 0,
+   yellow: 0,
+   blue: 0,
+   green: 0,
+}
+
+$(function(){//document ready
+  $('button').click(function(){ //listens for click on button
+    var color = $(this).data('color');//retrieves color data and stores it in a variable
+    var $block = $('<div class="color-cube '+color+'"></div')//creates div with class of the color value
+    $('.container').append($block);//appends div to DOM
+    colorCount[color]++;//increments color counter
+    $('#'+color).text(colorCount[color]);//updates color count text on DOM
   })
 });
